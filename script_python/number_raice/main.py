6ed6b35a3ab9202c95634657b152ef81027a27bd
fernando <fernandovillotaeraso@gmail.com>
2024-10-11 20:02:32 -0500

    dsd

--- script_python/number_raice/main.py
+++ script_python/number_raice/main.py
@@ -4,17 +4,17 @@ dev:Fernando  Vi
 date: 13/09/2024
 '''
 from random import randint 
-dice1 = randint(1,6)
-dice2 = randint(1,6)
+
 
 def main_menu():
-    print("::MAIN MENU::")
-    
+    print("::MAIN MENU::")    
     print("[1]. Start the game")
     print("[2]. About us")
     print("[3]. Exit")
     opt =int(input("Press any option: "))
     
+dice1 = randint(1,6)
+dice2 = randint(1,6)
 
 
 print (f"Dice1: {dice1} ")
