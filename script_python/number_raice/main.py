52b5b711996dc3ee4a5c6c560fadd66f6a677474
fernando <fernandovillotaeraso@gmail.com>
2024-10-11 20:10:15 -0500

    enter

--- script_python/number_raice/main.py
+++ script_python/number_raice/main.py
@@ -4,6 +4,7 @@ dev:Fernando  Vi
 date: 13/09/2024
 '''
 from random import randint 
+status_game=True
 
 
 def main_menu():
@@ -13,6 +14,21 @@ def main_menu():
     print("[3]. Exit")
     opt =int(input("Press any option: "))
     
+   
+
+    
+
+while status_game:
+    main_menu() 
+    op = main_menu() 
+    if op == 1:
+        print ("Game under construction")
+        key = ("Press any key to go to the main menu...")
+          
+       
+       
+# Generate two random numbers between 1 and 6
+        
 dice1 = randint(1,6)
 dice2 = randint(1,6)
 
